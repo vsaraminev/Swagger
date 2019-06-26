79b185168bbf05ff69d5dfd755a98f042b498592
vsaraminev <v.saraminev@abv.bg>
2019-06-26 23:04:48 +0300

    Add Definitions

---
+++ routes/order.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const authCheck = require('../middleware/auth-check');
+const Order = require('../models/Order');
+
+const router = new express.Router()
+
+router.post('/add/:partId', authCheck, async (req, res) => {
+    const partId = req.params.partId;
+    const userId = req.user._id;
+
+    // let part = await Part.findById(partId)
+    // .then((data)=>{
+    //     Order.create({
+    //         buyer: user,
+    //         part: data
+    //     }).then(() => {
+    //         res.status(200).json({
+    //             success: true,
+    //             message: 'Part added successfully to cart.'
+    //         })
+    //     })
+    // })
+
+    Order.create({
+        buyer: userId,
+        part: partId
+    })
+        .then(() => {
+            res.status(200).json({
+                success: true,
+                message: 'Part added successfully to cart.'
+            })
+        })
+})
+
+router.get('/user', authCheck, async (req, res) => {
+    const user = req.user._id
+
+    await Order.find({ buyer: user })
+        .populate('part')
+        .then((data) => {
+            return res.status(200).json(data)
+        })
+})
+
+module.exports = router
