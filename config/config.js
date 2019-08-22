96e5d186625dfb0fcdf1b437b0e05b9c6063786b
v.saraminev <v.saraminev@abv.bg>
2019-08-22 18:19:31 +0300

    Initial Doccker version

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     port: process.env.PORT || 5000,
-    dbPath: 'mongodb://localhost:27017/tuning-system'
+    dbPath: process.env.DB_PATH || 'mongodb://localhost:27017/tuning-system'
   },
   production: {}
 };
